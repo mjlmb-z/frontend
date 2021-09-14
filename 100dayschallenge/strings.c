d741a7c931a12b8ce611580346d7c9b028ea49cf
Lily <liklasinha11@gmail.com>
2021-09-14 16:36:55 +0530

    commit

--- 100dayschallenge/strings.c
+++ 100dayschallenge/strings.c
@@ -1,11 +1,21 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-    char s[10];
+    char s1[100],s2[100];
     printf("\nEnter your name:\t");
-    gets(s);
-    printf("Your name is %s",s);
+    gets(s1);
+    printf("\nYour name is %s",s1);
 
+    
+    strcpy(s2,s1);
+    printf("\n\nThe copied string:\t%s",s2);
+
+    strrev(s1);
+    printf("\n\nThe string after reversing:\t%s",s1);
+
+    int l=strlen(s1);
+    printf("\n\nThe length of the string is:\t%d",l);
     return 0;
 
 }
