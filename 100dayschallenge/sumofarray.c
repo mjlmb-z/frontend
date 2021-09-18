40641222992d6b468ce792277a12f5808af30b19
Lily <liklasinha11@gmail.com>
2021-09-18 21:33:40 +0530

    commit

---
+++ 100dayschallenge/sumofarray.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x[100], i, sum=0,s;
+    printf("\nEnter the size of the array:\t");
+    scanf("%d",&s);
+    printf("\nEnter %d elements:\n",s);
+    for(i=0;i<s;++i)
+    {
+        scanf("%d\n",&x[i]);
+        sum=sum+x[i];
+    }
+    printf("\nSum of the elements in the array:\t%d",sum);
+}
