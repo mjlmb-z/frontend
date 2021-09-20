ffbe86d651b8eb5aecda2b543b4802ffccf4eb16
Lily <liklasinha11@gmail.com>
2021-09-20 18:29:49 +0530

    commit

---
+++ 100dayschallenge/hello.js
@@ -0,0 +1,29 @@
+//HELLO WORLD
+//console.log("hello world!")
+//alert("hello world!")
+
+//interpretor; line by line execution.
+
+//VARIABLES
+
+//var myname= 'bat';
+//console.log(myname);
+
+//>>let(introduce a var): pros and cons
+//let newName='cat'
+//console.log(newName)>>cat (line nos)
+
+//const anotherName='rat';
+//console log(anotherName)>>stricter than var and let; 
+//needs to initialize values
+
+//var do not have a type as in c, c++. But the values have a type.
+//js is loosely typed language
+
+//newName=8;
+//console log("this is line 47",newName); 
+//object constructing and destructive
+
+//primitive values are immutable.
+//>>console.log(newName[0])
+//newName[0]='b';
