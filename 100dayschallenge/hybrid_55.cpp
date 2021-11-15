451e2ece212ec1e24a6746424a0bd628316bbd18
Lily <liklasinha11@gmail.com>
2021-11-15 21:02:07 +0530

    commit

---
+++ 100dayschallenge/hybrid_55.cpp
@@ -0,0 +1,23 @@
+//55 IT
+#include<iostream>
+using using namespace std;
+
+class student{
+
+};
+
+class test: public student{
+
+};
+
+class sports{
+
+};
+
+class results: public test,public sports{
+
+};
+
+int main(){
+    
+}
