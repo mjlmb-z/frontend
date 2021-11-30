f112a8c7216335fa2117c0c543f09f30b06d6b33
Lily <liklasinha11@gmail.com>
2021-12-01 00:34:25 +0530

    commit

--- nlv java/new/src/HelloController.java
+++ nlv java/new/src/HelloController.java
@@ -1,14 +1,36 @@
+package controller;
 
-
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button cancelButton;
+    @FXML
+    private Label loginMessageLabel;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField passwordPasswordField;
+
+    public void loginButtonOnAction(ActionEvent e) {
+
+        if (usernameTextField.getText().isBlank() == false && passwordPasswordField.getText().isBlank() == false) {
+            loginMessageLabel.setText("You try to login!");
+        } else {
+            loginMessageLabel.setText("Please enter username and password.");
+        }
+    }
+
+    public void cancelButtonOnAction(ActionEvent e){
+    Stage stage =(Stage) cancelButton.getScene().getWindow();
+    stage.close();
     }
 }
+
