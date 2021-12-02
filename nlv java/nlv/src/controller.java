e4af03542a1a117e7558a7b8b43c2f2b6ccaad25
Lily <liklasinha11@gmail.com>
2021-12-02 18:03:17 +0530

    commit

---
+++ nlv java/nlv/src/controller.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class controller extends Application {
+	
+	@Override
+	public void start(Stage stage) throws IOException {
+       
+		Parent root = FXMLLoader.load(getClass().getResource("Scene.fxml"));
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		
+		stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+
+			@Override
+			public void handle(WindowEvent arg0) {
+				
+				Platform.exit();
+				System.exit(0);	
+			}		
+		});
+	}	
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
