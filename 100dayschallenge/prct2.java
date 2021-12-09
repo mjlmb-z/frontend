b9a33dd065a43f54c1bad630070ea2f068d29388
Lily <liklasinha11@gmail.com>
2021-12-09 22:57:51 +0530

    commit

---
+++ 100dayschallenge/prct2.java
@@ -0,0 +1,9 @@
+//Write a java program to demonstrate java string buffer functions
+
+public class prct2 {
+    public static void main(String args[]){  
+    StringBuffer sb=new StringBuffer("Hello ");  
+    sb.append("Java");//now original string is changed  
+    System.out.println(sb);//prints Hello Java  
+    }  
+}
