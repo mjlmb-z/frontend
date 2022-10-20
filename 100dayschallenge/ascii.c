7017c7dd11fe8024b15f1adee93e784596de78da
Likla Sinha <89774560+mjlmb-z@users.noreply.github.com>
2022-10-20 20:54:01 +0530

    deletion

--- 100dayschallenge/ascii.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    char c;
-    printf("Enter a character:\t");
-    scanf("%c",&c);  
-    printf("\nASCII value of %c =\t%d", c, c);
-    return 0;
-}
--- 100dayschallenge/prct3.java
+++
@@ -1,6 +0,0 @@
-//Write a java program to do file IO
-
-import java.io.File;
-public class prct3 {
-    
-}
